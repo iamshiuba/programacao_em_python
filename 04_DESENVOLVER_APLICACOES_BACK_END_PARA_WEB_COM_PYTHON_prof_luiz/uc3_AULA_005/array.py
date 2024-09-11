2a519e0c26251f5823a9ea7334beee753057a7da
iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
2024-09-11 22:46:26 +0000

    att

--- 04_DESENVOLVER_APLICACOES_BACK_END_PARA_WEB_COM_PYTHON_prof_luiz/uc3_AULA_005/array.py
+++ 04_DESENVOLVER_APLICACOES_BACK_END_PARA_WEB_COM_PYTHON_prof_luiz/uc3_AULA_005/array.py
@@ -5,4 +5,13 @@ array / vetor / conjunto / colecao == lista
 vetor = []
 vetor.append(5)
 vetor.append(7)
+print(type(vetor))
 print(vetor)
+
+vetor.append(4.3)
+vetor.append("Renato")
+print(vetor)
+print(f"Quantidade de elementos: {len(vetor)}")
+
+# Primeira posição começa em 0
+print(vetor[0])
