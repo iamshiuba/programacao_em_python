0071dad828817c2cd5943ef8bb0eb5f7e97fc1dd
iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
2024-09-16 20:50:25 -0300

    Update exemplo_produtos.py

--- 04_DESENVOLVER_APLICACOES_BACK_END_PARA_WEB_COM_PYTHON_prof_luiz/uc3_AULA_007/exemplo_produtos.py
+++ 04_DESENVOLVER_APLICACOES_BACK_END_PARA_WEB_COM_PYTHON_prof_luiz/uc3_AULA_007/exemplo_produtos.py
@@ -3,10 +3,11 @@ produtos = []
 while True:
     nome = input("Nome do produto: ")
     preco = int(input("Preço: "))
-    produtos.append(nome, preco)
+    categoria = input("Categoria: ")
+    produtos.append([nome, preco, categoria])
     r = input("Deseja continuar? ")
     if r == 'n' or r == 'N':
         break
 
 for i in produtos:
-    print(f'{i[0]} - {i[1]}')
+    print(f'Produto: {i[0]}, preço: {i[1]}, categoria: {i[2]}')
