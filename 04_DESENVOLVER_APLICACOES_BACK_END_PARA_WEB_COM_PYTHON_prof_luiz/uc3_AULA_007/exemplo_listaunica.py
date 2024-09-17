feada6e4b49e9825795051c817a850686510d704
ishiuba <iamshiuba@hotmail.com>
2024-09-16 23:01:28 -0300

    fix

--- 04_DESENVOLVER_APLICACOES_BACK_END_PARA_WEB_COM_PYTHON_prof_luiz/uc3_AULA_007/exemplo_listaunica.py
+++ 04_DESENVOLVER_APLICACOES_BACK_END_PARA_WEB_COM_PYTHON_prof_luiz/uc3_AULA_007/exemplo_listaunica.py
@@ -1,11 +1,11 @@
 produtos = []
 
 while True:
-    nome = input ("Nome do produto: ")
+    nome = input("Nome do produto: ")
     preco = int(input('Preço: '))
     produtos.append(nome)
     produtos.append(preco)
-    r = input ("Deseja continuar? ")
+    r = input("Deseja continuar? ")
     if r == 'n' or r == 'N':
         break
 
