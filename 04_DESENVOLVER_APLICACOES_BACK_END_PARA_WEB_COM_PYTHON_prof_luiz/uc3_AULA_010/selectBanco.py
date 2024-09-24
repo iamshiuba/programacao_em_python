44a0ce916daed3f3abd47301ee242b4942e83565
sekiun-san <ian.pazeamor@outlook.com>
2024-09-23 21:19:41 -0300

    AULA 10
    banco de dados

---
+++ 04_DESENVOLVER_APLICACOES_BACK_END_PARA_WEB_COM_PYTHON_prof_luiz/uc3_AULA_010/selectBanco.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+conexao = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '',
+    database = 'escola'
+)
+
+cursor = conexao.cursor()
+
+sql = "select * from alunos"
+cursor.execute(sql)
+resultado = cursor.fetchall()
+cursor.close()
+conexao.close()
+
+for r in resultado:
+    print(r)
