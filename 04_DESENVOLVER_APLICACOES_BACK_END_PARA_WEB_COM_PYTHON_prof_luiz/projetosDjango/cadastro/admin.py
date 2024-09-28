f6425543f3a3f975c20e0a7093f59744761a951d
sekiun-san <ian.pazeamor@outlook.com>
2024-09-27 21:30:48 -0300

    AULA 12

--- 04_DESENVOLVER_APLICACOES_BACK_END_PARA_WEB_COM_PYTHON_prof_luiz/projetosDjango/cadastro/admin.py
+++ 04_DESENVOLVER_APLICACOES_BACK_END_PARA_WEB_COM_PYTHON_prof_luiz/projetosDjango/cadastro/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Curso, Aluno, Professor
 
 # Register your models here.
+admin.site.register(Curso)
+admin.site.register(Aluno)
+admin.site.register(Professor)
--- 04_DESENVOLVER_APLICACOES_BACK_END_PARA_WEB_COM_PYTHON_prof_luiz/projetosDjango/cadastro/models.py
+++ 04_DESENVOLVER_APLICACOES_BACK_END_PARA_WEB_COM_PYTHON_prof_luiz/projetosDjango/cadastro/models.py
@@ -4,3 +4,24 @@ from django.db import models
 class Curso(models.Model):
     nome = models.CharField(max_length=50)
     valor = models.DecimalField(max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
+
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Professor(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    matricula = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.nome
