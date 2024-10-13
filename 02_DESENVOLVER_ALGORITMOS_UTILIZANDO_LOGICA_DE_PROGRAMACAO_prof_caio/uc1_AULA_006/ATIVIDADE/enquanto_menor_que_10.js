d16afb3d957a81ef66dbf718fec21841fb88f50b
iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
2024-10-13 16:24:00 -0300

    rename uc3

--- 02_DESENVOLVER_ALGORITMOS_UTILIZANDO_LOGICA_DE_PROGRAMACAO_prof_caio/uc1_AULA_006/ATIVIDADE/enquanto_menor_que_10.js
+++
@@ -1,7 +0,0 @@
-// 1 - Obrigatoriamente utilizando o laço de repetição "while" faça com que um valor chamado "Contador" vá de 0 até 9, imprimindo cada vez que o valor incrementar.
-// 2 - Crie uma variável chamada contador e atribua o valor 0;
-// 3 - Escreva um WHILE com uma condição.
-// Obs: A tradução de WHILE significa ENQUANTO.
-// 4 - Vamos utilizar um operador "<" que é o operador de MENOR.
-// 5 - Dentro do WHILE deve ter um contador++ que vai incrementando o contador.
-// 6 - Ao fim deve imprimir os valores de 0 até 9.
--- 02_DESENVOLVER_ALGORITMOS_UTILIZANDO_LOGICA_DE_PROGRAMACAO_prof_caio/uc1_AULA_006/ATIVIDADE/imprimir_valores_ate_50.js
+++
@@ -1,8 +0,0 @@
-// 1 - Obrigatoriamente crie um For Loop que rode e imprima o valor de 0 até 50;
-// 2 - O For Loop é um laço de repetição.
-// 3 - Ele deve ter um "contador".
-// 4 - A cada "iteração" ele deve incrementar o contador caso seja correta a condição.
-
-for (contador = 0; contador < 50; contador++) {
-  console.log(contador);
-}
